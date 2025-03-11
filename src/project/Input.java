c1d3a078ee872a6a96504891a301a1cd61271716
Rizblack-89 <rizwannazar62@gmail.com>
2025-03-11 14:08:36 +0530

    new commit

--- src/project/Input.java
+++ src/project/Input.java
@@ -4,8 +4,7 @@ public class Input {
 	public static void main(String[] args) {
 		System.out.println("color");
 		System.out.println("black");
-
-		System.out.println("pink");
+                System.out.println("violet");
 
 	}
 }
