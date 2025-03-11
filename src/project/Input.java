89b7fbc7fb2aa2ca2bbd7912c0ac9c3b8826a466
Rizblack-89 <rizwannazar62@gmail.com>
2025-03-11 13:19:30 +0530

    3rd commit

--- src/project/Input.java
+++ src/project/Input.java
@@ -3,8 +3,8 @@ package project;
 public class Input {
 	public static void main(String[] args) {
 		System.out.println("color");
-		System.out.println("black");
-		System.err.println("yellow");
+	System.out.println("black");
+	System.err.println("yellow");
 		
 
 	}
